59f27db3c5d4a4cbfd0cab8d86fbbc7944895d21
Wanhong HUANG <huang-wanhong.g.official@gmail.com>
2026-03-07 18:57:11 +0800

    add submodules.

---
+++ versions.mk
@@ -0,0 +1,8 @@
+# --- Third-party Library Versions ---
+ABSL_VER     := 20250512.1
+PROTOBUF_SUPPORTED_VERS := v3.19.4 v33.5
+BOOST_VER    := 1.90.0
+OPENCV_VER   := 4.12.0
+OPENPOSE_VER := v1.7.0
+CAFFE_VER    := ver-openpose
+
